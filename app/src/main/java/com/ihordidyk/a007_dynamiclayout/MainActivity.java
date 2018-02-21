a1d5333c5267e8210a5a5389437e2681843b8e24
Ihor Didyk <ihordidyk95@gmail.com>
2018-02-21 12:59:14 +0200

    created app

---
+++ app/src/main/java/com/ihordidyk/a007_dynamiclayout/MainActivity.java
@@ -0,0 +1,68 @@
+package com.ihordidyk.a007_dynamiclayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout llMain;
+    RadioGroup rgGravity;
+    EditText etName;
+    Button btnCreate;
+    Button btnClear;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        llMain = findViewById(R.id.llMain);
+        rgGravity = findViewById(R.id.rgGravity);
+        etName = findViewById(R.id.etName);
+
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
+
+        btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCreate:
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                int btnGravity = 0;
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+                lParams.gravity = btnGravity;
+
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew, lParams);
+
+                break;
+            case R.id.btnClear:
+                llMain.removeAllViews();
+                Toast.makeText(this, "Deleted all elements", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
